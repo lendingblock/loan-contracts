9a13c0bc8d304d4ebcfbdf8762697a86dfdb5878
Julien Klepatch <julien@julienklepatch.com>
2018-06-08 13:40:46 +0800

    Enabled solc optimization

--- truffle.js
+++ truffle.js
@@ -8,6 +8,13 @@ module.exports = {
       gasPrice: 11
     }
   },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+    }
+  },
   solc: {
     optimizer: {
       enabled: true,
