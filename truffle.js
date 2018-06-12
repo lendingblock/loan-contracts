1b862d6f796f4c6d25a65d1604c8977e0afa6f1b
QX <macbook@QXs-MacBook-Pro.local>
2018-06-12 21:17:06 +0800

    add gas reporter
    https://github.com/lendingblock/loan-contracts/issues/22

--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 11
+    }
+  }
 };
